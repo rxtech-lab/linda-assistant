6a1b49416e23c53456390ab1c6b2fe3fbd42b33c
sirily11 <32106111+sirily11@users.noreply.github.com>
2026-02-13 23:09:46 +0800

    feat: add firecrawl mcp (#26)
    ---
    
    **Linked Issues:**
    Closes #22

---
+++ backend/lib/ai/tools/mcps/firecrawl.ts
@@ -0,0 +1,38 @@
+import { createMCPClient } from "@ai-sdk/mcp";
+
+/**
+ *
+ * @param accessToken Same Oauth token used to authenticate to api server
+ * @param needsApproval A mapping of tool names to whether they need approval
+ * @returns
+ */
+export const createFirecrawlMcp = async (
+  accessToken: string,
+  needsApproval: Record<string, boolean>,
+) => {
+  if (process.env.IS_E2E?.toLowerCase() === "true") {
+    return {};
+  }
+  const mcpUrl = process.env.FIRECRAWL_MCP_URL;
+  if (!mcpUrl) {
+    throw new Error("FIRECRAWL_MCP_URL is not defined");
+  }
+  const httpClient = await createMCPClient({
+    transport: {
+      type: "http",
+      url: mcpUrl,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  });
+
+  const tools = await httpClient.tools();
+
+  // every tool in the mcp needs to be approvaled before use
+  for (const [name, tool] of Object.entries(tools)) {
+    tool.needsApproval = needsApproval[name] ?? true;
+  }
+
+  return tools;
+};
